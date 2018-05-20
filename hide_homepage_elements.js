061fcdf2294534ce811f4b1f68747002aa87f8d7
A.J. Bale <abalejr@gmail.com>
2018-05-20 15:45:30 -0500

    Add function to hide 2 target elements

---
+++ hide_homepage_elements.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementsByClassName("sub-header")[0].classList.add('hidden');
+	document.getElementsByClassName("page-header")[0].classList.add('hidden');
+});
